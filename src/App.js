a11d01ecb735dfad4db9e70f8f7f3ec63d4dd0ba
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-08-21 14:57:53 +0200

    CSS Added

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ useEffect(() => {
 },[running]);
 
   return (
-    <><h1>Stopwatch</h1> 
-    <div>
+    <><h1>STOPWATCH</h1> 
+    <div class="time-display">
       <span>{("0" + Math.floor((time/60000) % 60)).slice(-2)}: </span> 
       <span>{("0" + Math.floor((time/1000) % 60)).slice(-2)}: </span>
       <span>{("0" + ((time/10) % 100)).slice(-2)}: </span>  
     </div>
-    <div>
+    <div class="button-display" >
       <button onClick={()=>{setRunning(true)}}>Start</button>
       <button onClick={()=>{setRunning(false)}}>Stop</button>
       <button onClick={()=> {setTime(0)}} >Reset</button>
