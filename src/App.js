2896b026ac8bab15ecb6436345b2b64c19f05d8f
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-08-21 14:42:25 +0200

    final steps , added a useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import {useState} from "react"; // useState is a react Hook , a react trick that helps track data / properties in between function calls.we see the return value from useState
+import {useEffect, useState} from "react"; // useState is a react Hook , a react trick that helps track data / properties in between function calls.we see the return value from useState
 
 function App() {
   const [time, setTime] = useState(0); // time is current state 2nd value is the function to update our state, the 0 is the initial state 
-const [running, setRunning] = useState(false);
+  const [running, setRunning] = useState(false);
+
+useEffect(() => {
+  let interval;
+  if (running) {
+    interval = setInterval(() => {
+      setTime((prevTime)  => prevTime + 10);
+    }, 10);
+  } else if (!running) {
+    clearInterval(interval);
+  }
+  return () => clearInterval(interval);
+},[running]);
+
   return (
     <><h1>Stopwatch</h1> 
     <div>
-      <span>{("0" + Math.floor((time/60000) % 60))}: </span> 
-      <span>{("0" + Math.floor((time/1000) % 60))}: </span>
-      <span>{("0" + ((time/10) % 100))}: </span>  
+      <span>{("0" + Math.floor((time/60000) % 60)).slice(-2)}: </span> 
+      <span>{("0" + Math.floor((time/1000) % 60)).slice(-2)}: </span>
+      <span>{("0" + ((time/10) % 100)).slice(-2)}: </span>  
     </div>
     <div>
       <button onClick={()=>{setRunning(true)}}>Start</button>
